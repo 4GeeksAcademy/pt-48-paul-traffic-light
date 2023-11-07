970d1de4c1f9fac00c8f750c337807cd539d8ea1
Paulmak21 <paulgrozescu@gmail.com>
2023-11-07 14:13:57 +0100

    Creating Function TrafficLight component/home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,93 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const colors = {
+  red: 'red',
+  yellow: 'yellow',
+  green: 'green',
 };
 
-export default Home;
+export default function TrafficLight() {
+  const [activeColor, setActiveColor] = useState(colors.red);
+  const [isActive, setIsActive] = useState(true);
+
+  useEffect(() => {
+    if (isActive) {
+      const timer = setInterval(() => {
+        switch (activeColor) {
+          case colors.red:
+            setActiveColor(colors.green);
+            break;
+          case colors.yellow:
+            setActiveColor(colors.red);
+            break;
+          case colors.green:
+            setActiveColor(colors.yellow);
+            break;
+          default:
+            setActiveColor(colors.red);
+        }
+      }, 2000);
+
+      return () => {
+        clearInterval(timer);
+      };
+    }
+  }, [activeColor, isActive]);
+
+  const toggleTrafficLight = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          background: '#2A2B2A',
+          borderRadius: '10px',
+          padding: '15px',
+          position: 'absolute',
+          top: '15%',
+          left: '50%'
+        }}
+      >
+        <div
+          style={{
+            width: '100px',
+            height: '100px',
+            background: colors.red,
+            borderRadius: '50%',
+            margin: '10px',
+            opacity: isActive && activeColor === colors.red ? '1' : '0.5',
+            boxShadow: activeColor === colors.red ? '0 0 20px red' : 'none',
+          }}
+        ></div>
+        <div
+          style={{
+            width: '100px',
+            height: '100px',
+            background: colors.yellow,
+            borderRadius: '50%',
+            margin: '10px',
+            opacity: isActive && activeColor === colors.yellow ? '1' : '0.5',
+            boxShadow:
+              activeColor === colors.yellow ? '0 0 20px yellow' : 'none',
+          }}
+        ></div>
+        <div
+          style={{
+            width: '100px',
+            height: '100px',
+            background: colors.green,
+            borderRadius: '50%',
+            margin: '10px',
+            opacity: isActive && activeColor === colors.green ? '1' : '0.5',
+            boxShadow: activeColor === colors.green ? '0 0 20px green' : 'none',
+          }}
+        ></div>
+        <button onClick={toggleTrafficLight}>
+          {isActive ? 'Turn Off' : 'Turn On'}
+        </button>
+      </div>
+    </>
+  );
+}
